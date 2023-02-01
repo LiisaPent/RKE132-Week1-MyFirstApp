b3808d0542ef4fb21a340ee25aa07c108571a151
Liisa <liisa.pent@tktk.ee>
2023-02-01 21:11:14 +0200

    valmis

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// this is a comment to my code
+
+// rakendus küsib kasutaja nime
+// rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");  // output - kõik, mida kuvame
+string userName = Console.ReadLine();  // input - kõik, mida sisestame
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+// string interpolation
+Console.WriteLine($"Hello, {userName}!");
+
